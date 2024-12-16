a91718b9feeefe3852f9fad5a1f83a268caad8a6
Serhii <sergijba@gmail.com>
2024-12-16 23:29:50 +0100

    Story and Features components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import Hero from './components/hero'
 import About from './components/About'
 import NavBar from './components/NavBar'
+import Features from './components/Features'
+import Story from './components/Story'
+import Contact from './components/Contact'
 
 const App = () => {
   return (
@@ -9,6 +12,9 @@ const App = () => {
       <NavBar />
       <Hero />
       <About />
+      <Features />
+      <Story />
+      <Contact />
 
     </main>
   )
